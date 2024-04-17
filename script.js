01cd3bfab995daaf9f565d59404daca84405b571
narendranandagiri <138883107+narendranandagiri@users.noreply.github.com>
2024-04-18 03:02:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function change() {
+    const menu1 = document.querySelector(".mobile-menu");
+    const icon1 = document.querySelector(".icon");
+    menu1.classList.toggle("open");
+    icon1.classList.toggle("open");
+  }
+  
